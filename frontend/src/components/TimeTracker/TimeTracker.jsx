5f2e20ca1dba45fa437f946748f04623141136f7
Atharva Parkale <atharva.parkale@gmail.com>
2023-06-18 20:15:41 +0530

    Added the api url to frontend

--- frontend/src/components/TimeTracker/TimeTracker.jsx
+++ frontend/src/components/TimeTracker/TimeTracker.jsx
@@ -34,24 +34,15 @@ function TimeTracker() {
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   }
 
-  console.log(projectTitle);
-
   const fetchNotes = async () => {
     try {
-      const { data } = await axios.get("http://localhost:4003/api/v1/sessions");
-      // console.log(data);
-
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/sessions`);
       const formattedTime = convertMillisecondsToHMS(data.sumOfTime);
       setFormattedTotalTime(formattedTime);
-
-      // setMyData(data.groupedData);
-      // setMyData(data.groupedData);
       setMyData(data.groupedDataTwo);
-
       if (data.sessions.length === 0) {
         setIsLoading(false);
       }
-
       setIsLoading(false);
       return data;
     } catch (err) {
@@ -63,7 +54,6 @@ function TimeTracker() {
     try {
       var today = new Date();
       var dd = today.getDate();
-
       var mm = today.getMonth() + 1;
       var yyyy = today.getFullYear();
 
@@ -76,9 +66,11 @@ function TimeTracker() {
       }
 
       today = mm + "/" + dd + "/" + yyyy;
+      console.log(process.env.REACT_APP_API)
 
       const { data } = await axios.post(
-        "http://localhost:4003/api/v1/session/new",
+        // "http://localhost:4003/api/v1/session/new",
+        `${process.env.REACT_APP_API}/session/new`,
         {
           description: description,
           totalTime: t,
@@ -94,7 +86,6 @@ function TimeTracker() {
       setFormattedTotalTime(formattedTime);
 
       setDescription("");
-      // setMyData(data.groupedData);
       setMyData(data.groupedDataTwo);
     } catch (error) {
       console.log("hi");
@@ -105,15 +96,12 @@ function TimeTracker() {
   const handleDeleteSession = async (id) => {
     try {
       const { data } = await axios.delete(
-        `http://localhost:4003/api/v1/session/${id}`
+        `${process.env.REACT_APP_API}/session/${id}`
       );
 
       const formattedTime = convertMillisecondsToHMS(data.sumOfTime);
       setFormattedTotalTime(formattedTime);
-
-      // setMyData(data.groupedData);
       setMyData(data.groupedDataTwo);
-
       setIsLoading(false);
     } catch (err) {
       console.log("Error in deleting the session!");
@@ -160,13 +148,9 @@ function TimeTracker() {
         formatTime(time.minutes) +
         ":" +
         formatTime(time.seconds);
-
       tt = x;
       gt = milliseconds;
-
-      // console.log(gt);
       setTotalTime(x);
-
       setTime({ seconds: 0, minutes: 0, hours: 0 });
     }
 
@@ -186,15 +170,13 @@ function TimeTracker() {
       setFlag(!flag);
       et = formattedDate;
       st = startTime;
-
       handleAddSession(tt, st, et, gt);
     }
   };
-
   const formatTime = (value) => {
     return value.toString().padStart(2, "0");
   };
-
+ 
   useEffect(() => {
     fetchNotes().then((data) => {
       setIsLoading(false);
@@ -249,7 +231,7 @@ function TimeTracker() {
           className="tracker-sidebar"
           sx={{
             // border: "2px solid black",
-            display:{xs:"none",sm:"flex"},
+            display: { xs: "none", sm: "flex" },
             width: "16%",
             height: "100%",
             borderRight: "0.01rem solid #607D8B",
@@ -259,7 +241,7 @@ function TimeTracker() {
           className="tracker-main-container"
           sx={{
             // border: "2px solid black",
-            width: {xs:"99%",sm:"84%"},
+            width: { xs: "99%", sm: "84%" },
             height: "100%",
             backgroundColor: "white",
             display: "flex",
@@ -292,14 +274,14 @@ function TimeTracker() {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                flexDirection:{xs:"column",sm:"row"}
+                flexDirection: { xs: "column", sm: "row" },
               }}
             >
               <Box
                 className="tracker-description-container"
                 sx={{
                   // border: "1px solid black",
-                  width: {xs:"100%",sm:"55%"},
+                  width: { xs: "100%", sm: "55%" },
                   height: "100%",
                   display: "flex",
                   alignItems: "center",
@@ -318,8 +300,8 @@ function TimeTracker() {
               <Box
                 className="tracker-options-container"
                 sx={{
-                    // border: "1px solid black",
-                  width: {xs:"100%",sm:"45%"},  
+                  // border: "1px solid black",
+                  width: { xs: "100%", sm: "45%" },
                   height: "100%",
                   display: "flex",
                   alignItems: "center",
@@ -337,7 +319,6 @@ function TimeTracker() {
                     justifyContent: "center",
                   }}
                 >
-
                   <select
                     name="select-value"
                     id="select-project-title"
@@ -367,14 +348,14 @@ function TimeTracker() {
                   <SellOutlinedIcon
                     sx={{
                       color: "#808080b8",
-                      fontSize:{xs:"1.1rem",sm:"1.5rem"}
+                      fontSize: { xs: "1.1rem", sm: "1.5rem" },
                     }}
                   />
                   <Divider orientation="vertical" />
                   <CurrencyRupeeOutlinedIcon
                     sx={{
                       color: "#808080b8",
-                      fontSize:{xs:"1.1rem",sm:"1.5rem"}
+                      fontSize: { xs: "1.1rem", sm: "1.5rem" },
                     }}
                   />
                   <Divider orientation="vertical" />
@@ -389,7 +370,7 @@ function TimeTracker() {
                     alignItems: "center",
                     justifyContent: "center",
                     fontWeight: "500",
-                    fontSize: {xs:"14px",sm:"19px"},
+                    fontSize: { xs: "14px", sm: "19px" },
                   }}
                 >
                   {formatTime(time.hours)}:{formatTime(time.minutes)}:
@@ -414,7 +395,7 @@ function TimeTracker() {
                     }}
                     sx={{
                       backgroundColor: isRunning ? "#f53f3f" : "#03a9f4",
-                      fontSize:{xs:"0.6rem",sm:"0.8125rem"},
+                      fontSize: { xs: "0.6rem", sm: "0.8125rem" },
 
                       ":hover": {
                         backgroundColor: isRunning ? "#f53f3f" : "#03a9f4",
@@ -458,7 +439,7 @@ function TimeTracker() {
               sx={{
                 // border: "1px solid black",
                 width: "95%",
-                height: "90%",
+                height: "80%",
               }}
             >
               {isLoading ? (
@@ -498,7 +479,7 @@ function TimeTracker() {
                           variant="span"
                           sx={{
                             color: "#999",
-                            fontSize: {xs:"0.7rem",sm:"0.8rem"},
+                            fontSize: { xs: "0.7rem", sm: "0.8rem" },
                             marginLeft: "1.5vw",
                           }}
                         >
@@ -509,7 +490,7 @@ function TimeTracker() {
                             variant="span"
                             sx={{
                               color: "#999",
-                              fontSize: {xs:"0.7rem",sm:"0.8rem"},
+                              fontSize: { xs: "0.7rem", sm: "0.8rem" },
                               marginRight: "1vw",
                             }}
                           >
@@ -521,7 +502,7 @@ function TimeTracker() {
                             sx={{
                               marginRight: "2vw",
                               fontWeight: "500",
-                              fontSize: {xs:"0.9rem",sm:"1.1rem"},
+                              fontSize: { xs: "0.9rem", sm: "1.1rem" },
                             }}
                           >{`${Math.floor(day.grandTotal / (1000 * 60 * 60))
                             .toString()
@@ -555,7 +536,7 @@ function TimeTracker() {
                               sx={{
                                 color: "#03a9f4",
                                 fontWeight: "500",
-                                fontSize: {xs:"0.7rem",sm:"0.8rem"},
+                                fontSize: { xs: "0.7rem", sm: "0.8rem" },
                                 marginLeft: "1.5vw",
                               }}
                             >
@@ -566,7 +547,7 @@ function TimeTracker() {
                                 variant="span"
                                 sx={{
                                   color: "#999",
-                                  fontSize: {xs:"0.7rem",sm:"0.8rem"},
+                                  fontSize: { xs: "0.7rem", sm: "0.8rem" },
                                   marginRight: "1vw",
                                 }}
                               >
@@ -578,7 +559,7 @@ function TimeTracker() {
                                 sx={{
                                   marginRight: "2vw",
                                   fontWeight: "500",
-                                  fontSize: {xs:"0.9rem",sm:"1rem"},
+                                  fontSize: { xs: "0.9rem", sm: "1rem" },
                                   color: "#999",
                                 }}
                               >{`${Math.floor(
@@ -633,7 +614,11 @@ function TimeTracker() {
                                 >
                                   <input
                                     type="text"
-                                    value={session.description===""?"No description":session.Description}
+                                    value={
+                                      session.description === ""
+                                        ? "No description"
+                                        : session.Description
+                                    }
                                     onChange={() => {}}
                                   />
                                 </Box>
@@ -654,7 +639,7 @@ function TimeTracker() {
                                       // border: "1px solid black",
                                       height: "100%",
                                       width: "25%",
-                                      display: {xs:"none",sm:"flex"},
+                                      display: { xs: "none", sm: "flex" },
                                       alignItems: "center",
                                       justifyContent: "center",
                                     }}
@@ -682,11 +667,11 @@ function TimeTracker() {
                                     sx={{
                                       // border: "1px solid black",
                                       height: "100%",
-                                      width: {xs:"50%",sm:"38%"},
+                                      width: { xs: "50%", sm: "38%" },
                                       display: "flex",
                                       alignItems: "center",
                                       justifyContent: "space-around",
-                                      fontSize: {xs:"14px",sm:"19px"}
+                                      fontSize: { xs: "14px", sm: "19px" },
                                     }}
                                   >
                                     <Divider orientation="vertical" />
@@ -702,12 +687,12 @@ function TimeTracker() {
                                     sx={{
                                       // border: "1px solid black",
                                       height: "100%",
-                                      width: {xs:"30%",sm:"25%"},
+                                      width: { xs: "30%", sm: "25%" },
                                       display: "flex",
                                       alignItems: "center",
                                       justifyContent: "center",
                                       fontWeight: "500",
-                                      fontSize: {xs:"14px",sm:"19px"},
+                                      fontSize: { xs: "14px", sm: "19px" },
                                     }}
                                   >
                                     {session.totalTime}
